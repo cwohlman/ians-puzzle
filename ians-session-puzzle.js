19aa7eb97ddecf3bb0b6dadb609217972c2e37e5
Joshua Ohlman <codewriterohs@gmail.com>
2014-04-22 22:08:52 -0500

    Update ians-puzzle to allow two player play. Note that app does not persist user id yet.

--- ians-session-puzzle.js
+++ ians-session-puzzle.js
@@ -1,16 +1,18 @@
-users = new Meteor.Collection('players')
+players = new Meteor.Collection('players')
 games = new Meteor.Collection('games')
 
 if (Meteor.isClient) {
+  Meteor.subscribe('players');
+  Meteor.subscribe('games');
   Template.main.helpers({
     showWelcome: function () {
       return !Session.get('user_id');
     },
-    showLoby: function () {
-      return Session.get('user_id') && !Session.get('game_id');
+    showLobby: function () {
+      return Session.get('user_id') && !games.findOne();
     },
     showGame: function () {
-      return Session.get('game_id');
+      return games.findOne();
     }
   });
   Template.welcome.events({
@@ -19,6 +21,35 @@ if (Meteor.isClient) {
       var userName = tmpl.find('input').value;
       Meteor.call('signUp', userName, function (error, result) {
         Session.set('user_id', result);
+      });
+    }
+  });
+  Template.lobby.helpers({
+    availablePlayers: function () {
+      return players.find({
+        _id: {
+          $not: Session.get('user_id')
+        }
+      }).fetch();
+    }
+  });
+  Template.lobby.events({
+    'button click': function (e, tmpl) {
+      Meteor.call('startGame', this._id, function (error, result) {});
+    }
+  });
+  Template.game.helpers({
+    game: function () {
+      return games.findOne();
+    }
+  });
+  Template.game.events({
+    'submit form': function (e, tmpl) {
+      e.preventDefault();
+      var val = Number(tmpl.find('input').value);
+      var game_id = games.findOne()._id;
+      Meteor.call('submitMove', game_id, val, function (error, result) {
+        // TODO: validation feedback.
       });
     }
   });
@@ -33,26 +64,73 @@ if (Meteor.isServer) {
     //  total: 0
     // }
     submitMove: function (game_id, move) {
-      // TODO!
+      var current_user = Meteor.userId()
+      var game = games.findOne({
+        _id: game_id
+      });
+      var user_index = game.players.indexOf(current_user);
+      if (user_index == -1) {
+        throw new Error("User is not part of this game!")
+      }
+      var lastMove = game.moves && game.moves[game.moves.length - 1];
+      if (lastMove && lastMove.user_id == current_user) {
+        throw new Error("Not your turn.")
+      }
+      var newMove = {
+        user_id: current_user,
+        move: move
+      };
+      var total = game.total;
+      total += move;
+      game.moves = game.moves || []
+      game.moves.push(newMove);
+      games.update({
+        _id: game_id
+      },{
+        $set: {
+          moves: game.moves,
+          total: total
+        }
+      });
     },
     startGame: function (user_id) {
-      var current_user = this.userId()
-
+      var current_user = Meteor.userId();
+      return games.insert({
+        players: [
+          current_user,
+          user_id
+        ],
+        total: 0
+      })
     },
     signUp: function (userName) {
-      var user = users.findOne({
+      var user = players.findOne({
         userName: userName
       })
-      if (!user) {users.insert({
+      if (!user) {players.insert({
           userName: userName
         });
       }
       user = user._id || user;
+      this.setUserId(user);
       return user;
     },
     getUser: function () {
       return Meteor.userId()
     }
+  });
+  Meteor.publish('players', function () {
+    return players.find();
+  });
+  Meteor.publish('games', function () {
+    return games.find({
+      players: {
+        $in: [this.userId]
+      },
+      total: {
+        $lt: 50
+      }
+    })
   })
   Meteor.startup(function () {
     // code to run on server at startup
