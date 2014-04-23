f270ee3bfa822719280896f39d822975a389b1a6
Joshua Ohlman <codewriterohs@gmail.com>
2014-04-22 19:52:55 -0500

    Add logic to login anonymous users by name. (No password is required.)

--- ians-session-puzzle.js
+++ ians-session-puzzle.js
@@ -1,55 +1,59 @@
+users = new Meteor.Collection('players')
+games = new Meteor.Collection('games')
+
 if (Meteor.isClient) {
-  Meteor.startup(function () {
-    Session.set('total', Session.get('total') || 0)
-  })
-  Template.hello.greeting = function () {
-    return "Welcome to ians-session-puzzle.";
-  };
-
-  Template.hello.events({
-    'submit form': function (e, tmpl) {
-      e.preventDefault();
-      var val = Number($('input').val());
-      $('input').val('')
-      if (!val || val > 10 || val < 1) {
-        alert('Invalid selection');
-        return;
-      }
-      var total = Session.get('total')
-      if (total >= 50) {
-        total = 0;
-      }
-      total += val;
-      if (total == 50) {
-        $('.winner').text('Congratulations you are the winner!')
-      } else {
-        $('.winner').text('')
-        var computerpick = (50 - total) % 11;
-        if (computerpick == 0) computerpick = Math.round(Math.random() * 9 + 1);
-        total += computerpick
-        Session.set('computer', computerpick)
-        $('.computerpick').slideDown().delay(500).slideUp(400)
-      }
-
-      Session.set('total', total);
-
+  Template.main.helpers({
+    showWelcome: function () {
+      return !Session.get('user_id');
+    },
+    showLoby: function () {
+      return Session.get('user_id') && !Session.get('game_id');
+    },
+    showGame: function () {
+      return Session.get('game_id');
     }
   });
-
-  Template.hello.helpers({
-    'total': function () {
-      return Session.get('total');;
-    },
-    'goal':function () {
-      return 50;
-    },
-    'computer': function () {
-      return Session.get('computer');
+  Template.welcome.events({
+    'submit form': function (e, tmpl) {
+      e.preventDefault();
+      var userName = tmpl.find('input').value;
+      Meteor.call('signUp', userName, function (error, result) {
+        Session.set('user_id', result);
+      });
     }
-  })
+  });
 }
 
 if (Meteor.isServer) {
+  Meteor.publish('currentUser')
+  Meteor.methods({
+    // Returns a game object {
+    //  players: [],
+    //  moves: [],
+    //  total: 0
+    // }
+    submitMove: function (game_id, move) {
+      // TODO!
+    },
+    startGame: function (user_id) {
+      var current_user = this.userId()
+
+    },
+    signUp: function (userName) {
+      var user = users.findOne({
+        userName: userName
+      })
+      if (!user) {users.insert({
+          userName: userName
+        });
+      }
+      user = user._id || user;
+      return user;
+    },
+    getUser: function () {
+      return Meteor.userId()
+    }
+  })
   Meteor.startup(function () {
     // code to run on server at startup
   });
